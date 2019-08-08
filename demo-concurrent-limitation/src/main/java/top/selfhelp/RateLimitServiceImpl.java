25bfe8e72d5e8054b7a83ddcd413d977931338bf
zhuyuqiang <zhuyuqiang@cloudwalk.cn>
2019-08-08 19:25:48 +0800

    添加并发頻率控制相关注解和切面實現

--- demo-concurrent-limitation/src/main/java/top/selfhelp/RateLimitServiceImpl.java
+++ demo-concurrent-limitation/src/main/java/top/selfhelp/RateLimitServiceImpl.java
@@ -11,34 +11,39 @@ public class RateLimitServiceImpl {
 
     private RateLimiter rateLimiter = null;
 
-    private static double PERMITS_PER_SECOND = 0.01D;
+    private long timeout = 0L;
+
+    private TimeUnit timeUnit = TimeUnit.SECONDS;
+
+    private static double PERMITS_PER_SECOND = 1D;
 
     public RateLimitServiceImpl(){
-        this(PERMITS_PER_SECOND);
+        this(PERMITS_PER_SECOND,0L,TimeUnit.SECONDS);
     }
 
-    public RateLimitServiceImpl(double permitsPerSecond){
+    public RateLimitServiceImpl(double permitsPerSecond,long timeout,TimeUnit timeUnit){
         this.rateLimiter = RateLimiter.create(permitsPerSecond);
+        this.timeout = timeout;
+        this.timeUnit = timeUnit;
     }
 
-    public RateLimitServiceImpl(double permitsPerSecond,long warmupPeriod, TimeUnit unit ){
+    public RateLimitServiceImpl(double permitsPerSecond,long warmupPeriod, TimeUnit unit ,long timeout,TimeUnit timeUnit){
         this.rateLimiter = RateLimiter.create(permitsPerSecond,warmupPeriod,unit);
+        this.timeout = timeout;
+        this.timeUnit = timeUnit;
     }
 
-    public double acquire(){
-        return rateLimiter.acquire();
-    }
-
-    public double acquire(int permits){
-        return rateLimiter.acquire(permits);
-    }
-
-    public boolean acquire(long timeout, TimeUnit unit){
-        return rateLimiter.tryAcquire(timeout,unit);
-    }
-
-    public boolean acquire(int permits, long timeout, TimeUnit unit){
-        return rateLimiter.tryAcquire(permits,timeout,unit);
+    public double acquire() throws InterruptedException {
+        long start = System.currentTimeMillis();
+        if(timeout<=0){
+            return rateLimiter.acquire(1);
+        }else{
+            boolean result = rateLimiter.tryAcquire(1,timeout,timeUnit);
+            if(!result){
+                throw new InterruptedException("获取令牌超时");
+            }
+            return timeUnit.toSeconds(System.currentTimeMillis()-start);
+        }
     }
 
     public void setRate(double permitsPerSecond){
--- demo-concurrent-limitation/src/main/java/top/selfhelp/aop/RateLimitAspect.java
+++ demo-concurrent-limitation/src/main/java/top/selfhelp/aop/RateLimitAspect.java
@@ -42,21 +42,25 @@ public class RateLimitAspect {
             String name = StringUtils.isEmpty(rateLimit.name())?targetMethod.getName():rateLimit.name();
             logger.debug("当前限流参数：名称：{}，允许最大并行数量：{}，最大等待时间：{}，时间单位：{}",
                     name,rateLimit.permitsPerSecond(),rateLimit.timeout(),rateLimit.unit());
-            CountLimitServiceImpl countLimitService = countLimitMapper
+            RateLimitServiceImpl rateLimitService = rateLimitMapper
                     .get(name);
             //并发数量控制器不存在，新建并发控制器
-            if(countLimitService == null){
-                countLimitService = new CountLimitServiceImpl(countLimit.count(),false,countLimit.timeout(),countLimit.unit());
+            if(rateLimitService == null){
+                if(rateLimit.warmupMode()){
+                    rateLimitService = new RateLimitServiceImpl(rateLimit.permitsPerSecond(),rateLimit.warmupPeriod(),
+                            rateLimit.unit(),rateLimit.timeout(),rateLimit.timeoutUnit());
+                }else{
+                    rateLimitService = new RateLimitServiceImpl(rateLimit.permitsPerSecond(),rateLimit.timeout(),
+                            rateLimit.timeoutUnit());
+                }
             }
             long start = System.currentTimeMillis();
             logger.debug("当前请求线程id：{},尝试获取请求令牌时间：{}",Thread.currentThread().getId(),start);
-            countLimitService.acquire();
+            rateLimitService.acquire();
             logger.debug("当前请求线程id：{}，请求令牌等待时间：{}",Thread.currentThread().getId(),(System.currentTimeMillis()-start));
             //添加并发控制器到内存中，供下次获取
-            countLimitMapper.put(name,countLimitService);
-            Object result = joinPoint.proceed(joinPoint.getArgs());
-            countLimitService.release();
-            return result;
+            rateLimitMapper.put(name,rateLimitService);
+            return joinPoint.proceed(joinPoint.getArgs());
         }  catch (Throwable throwable) {
             logger.error("并发数量控制异常",throwable);
             throw new LimitationException(throwable);
